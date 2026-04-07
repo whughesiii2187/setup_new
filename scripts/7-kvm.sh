6d8c50f00d30290e688d2b929f016024244a8a2f
Bill Hughes <whughesiii@pm.me>
2026-04-06 23:33:45 -0500

    changed to just a setup shell script and clone appropriate dots

--- scripts/7-kvm.sh
+++ scripts/7-kvm.sh
@@ -9,17 +9,17 @@ echo "START KVM/QEMU/VIRT MANAGER INSTALLATION..."
 # ------------------------------------------------------
 # Install Packages
 # ------------------------------------------------------
-yay -S virt-manager virt-viewer qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf swtpm vim
+yay -S --needed libvirt virt-manager virt-viewer qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf swtpm
 
 # ------------------------------------------------------
 # Edit libvirtd.conf
 # ------------------------------------------------------
 # TODO: Use sed to remove the #
 echo "Manual steps required:"
-echo "Open sudo vim /etc/libvirt/libvirtd.conf:"
+echo "Open sudo nvim /etc/libvirt/libvirtd.conf:"
 echo 'Remove # at the following lines: unix_sock_group = "libvirt" and unix_sock_rw_perms = "0770"'
 read -p "Press any key to open libvirtd.conf: " c
-sudo vim /etc/libvirt/libvirtd.conf
+sudo nvim /etc/libvirt/libvirtd.conf
 sudo echo 'log_filters="3:qemu 1:libvirt"' >> /etc/libvirt/libvirtd.conf
 sudo echo 'log_outputs="2:file:/var/log/libvirt/libvirtd.log"' >> /etc/libvirt/libvirtd.conf
 
@@ -39,12 +39,12 @@ sudo systemctl start libvirtd
 # ------------------------------------------------------
 # TODO: Investigate using the defaults and adding users to the qemu group
 echo "Manual steps required:"
-echo "Open sudo vim /etc/libvirt/qemu.conf"
+echo "Open sudo nvim /etc/libvirt/qemu.conf"
 echo "Uncomment and add your user name to user and group."
 echo 'user = "your username"'
 echo 'group = "your username"'
 read -p "Press any key to open qemu.conf: " c
-sudo vim /etc/libvirt/qemu.conf
+sudo nvim /etc/libvirt/qemu.conf
 
 # ------------------------------------------------------
 # Restart Services
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ "$(uname)" = "Darwin" ]; then
+  git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
+  cd ~/dotfiles/
+  git sparse-checkout set macos
+  cd macos
+  stow -t ~ aerospace
+  stow -t ~ ghostty
+  stow -t ~ nvim
+  stow -t ~ tmux
+  stow -t ~ zshrc
+elif [ -f "/.dockerenv" ]; then
+  git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
+  cd ~/dotfiles/
+  git sparse-checkout set devc
+  cd devc
+  ./setup.sh
+elif [ $(uname) = "Linux" ]; then
+  git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
+  cd ~/dotfiles/
+  git sparse-checkout set linux
+  cd linux
+  stow -t ~ ghostty
+  stow -t ~ nvim
+  stow -t ~ tmux
+  stow -t ~ zshrc
+  if [ -d ~/.local/share/omarchy ]; then
+    stow -t ~ omarchy
+  fi
+fi
+
