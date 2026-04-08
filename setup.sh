090a118e60e6ed165e7e4dd167b9cedb6f2ca765
Bill Hughes <whughesiii@pm.me>
2026-04-08 16:52:21 -0500

    little scripting changes

--- setup.sh
+++ setup.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 
-if [ "$(uname)" = "Darwin" ]; then
-  git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
-  cd ~/dotfiles/
-  git sparse-checkout set macos
-  cd macos
-  stow -t ~ aerospace
-  stow -t ~ ghostty
-  stow -t ~ nvim
-  stow -t ~ tmux
-  stow -t ~ zshrc
-elif [ -f "/.dockerenv" ]; then
+if [ -f "/.dockerenv" ]; then
   git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
   cd ~/dotfiles/
   git sparse-checkout set devc
   cd devc
   ./setup.sh
-elif [ $(uname) = "Linux" ]; then
+else 
   git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
   cd ~/dotfiles/
-  git sparse-checkout set linux
-  cd linux
+  git sparse-checkout set default
+  cd default
   stow -t ~ ghostty 
   stow -t ~ nvim
   stow -t ~ tmux
   stow -t ~ zshrc
   if [ -d ~/.local/share/omarchy ]; then
     stow -t ~ omarchy
+  fi
+  if [ "$(uname)" = "Darwin" ]; then
+    stow -t ~ aerospace
+    stow -t ~ sketchybar
+    stow -t ~ macoszshrc
   fi
 fi
 
