6fc65d7e5fbc75b851f27082a64fd35dda0c43c5
Bill Hughes <whughesiii@pm.me>
2026-04-08 12:37:20 -0500

    changes to setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -d ~/.config/ghostty ]; then
-  rm -rf ~/.config/ghostty
-fi
-
 if [ "$(uname)" = "Darwin" ]; then
   git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
   cd ~/dotfiles/
