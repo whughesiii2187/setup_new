648c3bd8850e8165c8c4410288ad9774a4c410a9
Bill Hughes <whughesiii@pm.me>
2026-05-01 13:36:12 -0500

    checks and such

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d "/workspace" ]; then
+if [[ -d "/workspace" || -d ".devcontainer" ]]; then
   git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
   cd ~/dotfiles/
   git sparse-checkout set devc
