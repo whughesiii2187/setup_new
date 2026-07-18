466bb92f034ba6e5898a4cb32087e6b50e5833f3
whughesiii2187 <whughesiii@pm.me>
2026-07-18 18:26:16 -0500

    Update install_all.sh to modify package installation order
    Reorganize installation steps and remove redundant commands.

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -4,27 +4,15 @@
 ./install_ghostty.sh
 ./install_bitwarden.sh
 ./install_stow.sh
-./install_tmux.sh
 ./install_brew.sh
+./install_printer.sh
+./install_zsh.sh
 
 # Take care of some new Omarchy install prereqs
 ## Omarchy overrides ##
 if [ -d "$HOME/.config/omarchy" ]; then
   echo "source = ~/.config/hypr/omarchy_overrides.conf" >> ~/.config/hypr/hyprland.conf
   echo "source = ~/.config/hypr/omarchy_keybind_overrides.conf" >> ~/.config/hypr/hyprland.conf
-fi
-
-## Dotfiles
-# Check that ~/.dotfiles doesn't exist first
-rm -rf ~/.config/ghostty/ ~/.config/nvim/ ~/.config/tmux ~/.local/state/nvim/ ~/.local/share/nvim/ ~/.config/tmux
-
-./install_zsh.sh
-
-## Clone and Stow Dotfiles ##
-if [ -d "$HOME/dotfiles" ]; then
-  echo "Dotfiles appear to be installed already, skipping"
-else
-  ./install_dotfiles.sh
 fi
 
 # Allow CUPS through firewall
@@ -35,7 +23,16 @@ if [ -d "$HOME/.config/omarchy" ]; then
   bash <(curl -fsSL https://raw.githubusercontent.com/DanielCoffey1/a-la-carchy/master/a-la-carchy.sh)
 fi
 
-./install_printer.sh
+## Dotfiles
+# Check that ~/.dotfiles doesn't exist first
+rm -rf ~/.config/ghostty/ ~/.config/nvim/ ~/.config/tmux ~/.local/state/nvim/ ~/.local/share/nvim/ ~/.config/tmux ~/.zshrc
+## Clone and Stow Dotfiles ##
+if [ -d "$HOME/dotfiles" ]; then
+  echo "Dotfiles appear to be installed already, skipping"
+else
+  ./install_dotfiles.sh
+fi
+
+./install_tmux.sh
 # ./install_vpn.sh
 # ./install_gazelle.sh
-
