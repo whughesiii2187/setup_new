3773b91439a2a49a388e3a58cd987b04aefe3a8c
whughesiii2187 <whughesiii@pm.me>
2026-07-18 17:57:09 -0500

    Add -y flag to dnf commands in install script

--- scripts/install_ghostty.sh
+++ scripts/install_ghostty.sh
@@ -5,6 +5,6 @@ if command -v yay &> /dev/null; then
 fi
 
 if command -v dnf &> /dev/null; then
-  sudo dnf copr enable scottames/ghostty
-  sudo dnf install ghostty
+  sudo dnf copr enable -y scottames/ghostty
+  sudo dnf install -y ghostty
 fi
