c07b95adec7be939378be8f41dc3dd9ea3dd5939
whughesiii2187 <whughesiii@pm.me>
2026-07-18 20:47:34 -0500

    Refactor zshrc handling in setup.sh
    Removed redundant zshrc handling and added it after cloning dotfiles.

--- setup.sh
+++ setup.sh
@@ -8,25 +8,27 @@ if [[ -d "/workspace" || -d ".devcontainer" ]]; then
   ./setup.sh
 else
   git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   cd ~/dotfiles/
   git sparse-checkout set default
   cd default
   stow -t ~ ghostty
   stow -t ~ nvim
   stow -t ~ tmux
+
+  if [ -f "$HOME/.zshrc" ]; then
+    rm $HOME/.zshrc
+    stow -t ~ zshrc
+  fi
   if [ -d ~/.local/share/omarchy ]; then
     stow -t ~ omarchy
     rm -rf ~/.config/tmux
   fi
+   
   # OS Specific
   if [ "$(uname)" = "Darwin" ]; then
     stow -t ~ aerospace
     stow -t ~ sketchybar
     stow -t ~ macoszshrc
   fi
-  if [ -f "$HOME/.zshrc" ]; then
-    rm $HOME/.zshrc
-    stow -t ~ zshrc
-  fi
 fi
+
