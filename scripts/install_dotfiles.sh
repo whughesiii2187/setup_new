2c23a4a7f7b5e4b635b2e6e3d97ad877407a4b06
Bill Hughes <whughesiii@pm.me>
2026-08-20 22:56:25 -0500

    update stow script

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -2,11 +2,13 @@
 
 git clone --filter=blob:none --sparse https://github.com/whughesiii2187/dotfiles ~/dotfiles
 cd ~/dotfiles/
-git sparse-checkout set ghostty tmux zshrc omarchy nvim aerospace sketchybar zshrc-mac
+git sparse-checkout set ghostty tmux zshrc omarchy nvim aerospace sketchybar zshrc-mac scripts wireplumber
 
 stow -t ~ ghostty
 stow -t ~ nvim
 stow -t ~ tmux
+stow -t ~ wireplumber
+stow -t ~ scripts
 
 if [ -f "$HOME/.zshrc" ]; then
   rm $HOME/.zshrc
